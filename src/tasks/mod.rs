5966eabb224b6ba267837c08bc6dbdd18e1917ce
Vitaly Senko <senko.vitaly@gmail.com>
2024-12-29 01:47:07 +0100

    Rename folders.

--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -1,5 +1,10 @@
+#[path = "01_arrays/mod.rs"]
 mod arrays;
+#[path = "04_prefix_sum/mod.rs"]
 mod prefix_sum;
+#[path = "03_sliding_window/mod.rs"]
 mod sliding_window;
+#[path = "01_strings/mod.rs"]
 mod strings;
+#[path = "02_two_pointers/mod.rs"]
 mod two_pointers;
