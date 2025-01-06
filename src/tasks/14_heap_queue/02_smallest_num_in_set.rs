7b00ad38620387d4769714e523a51e6f77ba3e03
Vitaly Senko <senko.vitaly@gmail.com>
2025-01-06 21:44:03 +0100

    Max subseq score

--- src/tasks/14_heap_queue/02_smallest_num_in_set.rs
+++ src/tasks/14_heap_queue/02_smallest_num_in_set.rs
@@ -5,6 +5,7 @@
 //! int popSmallest() Removes and returns the smallest integer contained in the infinite set.
 //! void addBack(int num) Adds a positive integer num back into the infinite set, if it is not already in the infinite set.
 //! 1 <= num <= 1000
+//! https://leetcode.com/problems/smallest-number-in-infinite-set
 
 use std::{
     cmp::Reverse,
--- src/tasks/14_heap_queue/mod.rs
+++ src/tasks/14_heap_queue/mod.rs
@@ -3,3 +3,6 @@ mod k_largest_in_vec;
 
 #[path = "02_smallest_num_in_set.rs"]
 mod smallest_num_in_set;
+
+#[path = "03_max_subseq_score.rs"]
+mod max_subseq_score;
