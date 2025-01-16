863f6a263e69a9fe11d12500811b328a8d59ea35
Vitaly Senko <senko.vitaly@gmail.com>
2025-01-16 21:07:44 +0100

    Finally, longesst common subsequence.

--- src/tasks/18_dp_multidimensional/02_longest_com_seq.rs
+++ src/tasks/18_dp_multidimensional/02_longest_com_seq.rs
@@ -23,109 +23,31 @@ struct SharedSubSeqData {
 }
 
 impl Solution {
-    pub fn recursive_sub_seq_search(chars_1: &[char], chars_2: &[char]) -> usize {
-        let mut max_len: usize = 0;
+    pub fn longest_common_subsequence(s_1: String, s_2: String) -> i32 {
+        let s_1 = s_1.chars().collect::<Vec<char>>();
+        let s_2 = s_2.chars().collect::<Vec<char>>();
 
-        let shared_sub_seq_data = SharedSubSeqData {
-            max_seq_len: chars_2.len(),
-        };
+        let mut grid: Vec<Vec<usize>> = vec![vec![0; s_2.len()]; s_1.len()];
 
-        #[allow(clippy::needless_range_loop)]
-        fn _recursive_sub_seq_search<T>(
-            chars_1: &[char],
-            chars_2: &[char],
-            mut progress: CommonSubSeqProgress,
-            shared_sub_seq_data: &SharedSubSeqData,
-            on_end: &mut T,
-        ) where
-            T: FnMut(CommonSubSeqProgress),
-        {
-            if progress.s_1_idx >= chars_1.len()
-                || progress.s_2_idx >= chars_2.len()
-                || shared_sub_seq_data.max_seq_len == progress.seq_len
-            {
-                on_end(progress);
-                return;
-            }
-
-            let mut split: Vec<CommonSubSeqProgress> = Vec::with_capacity(2);
-
-            for i in progress.s_1_idx..chars_1.len() {
-                let c = chars_1[i];
-                if let Some(found_char_position) = chars_2
-                    .iter()
-                    .enumerate()
-                    .skip(progress.s_2_idx)
-                    .find(|(i, c2)| **c2 == c)
-                    .map(|(i, _)| i)
-                {
-                    // decided to use char
-                    split.push(CommonSubSeqProgress {
-                        s_1_idx: i + 1,
-                        s_2_idx: found_char_position + 1,
-                        seq_len: progress.seq_len + 1,
-                    });
-                    // decided not to use char
-                    split.push(CommonSubSeqProgress {
-                        s_1_idx: i + 1,
-                        s_2_idx: progress.s_2_idx,
-                        seq_len: progress.seq_len,
-                    });
-                    break;
+        // grid[i][j] is longest seq between s_1[0..=i] and s_2[0..=j]
+        for (i_1, c_1) in s_1.into_iter().enumerate() {
+            for (i_2, c_2) in s_2.iter().enumerate() {
+                grid[i_1][i_2] = if c_1 == *c_2 {
+                    let prev = if i_1 == 0 || i_2 == 0 {
+                        0
+                    } else {
+                        grid[i_1 - 1][i_2 - 1]
+                    };
+                    prev + 1
+                } else {
+                    let prev_1 = if i_1 == 0 { 0 } else { grid[i_1 - 1][i_2] };
+                    let prev_2 = if i_2 == 0 { 0 } else { grid[i_1][i_2 - 1] };
+                    prev_1.max(prev_2)
                 }
             }
-
-            // we have finished without finding new state
-            if split.is_empty() {
-                on_end(progress);
-                return;
-            }
-
-            for new_progress in split.into_iter() {
-                _recursive_sub_seq_search(
-                    chars_1,
-                    chars_2,
-                    new_progress,
-                    shared_sub_seq_data,
-                    on_end,
-                );
-            }
         }
 
-        _recursive_sub_seq_search(
-            chars_1,
-            chars_2,
-            CommonSubSeqProgress {
-                s_1_idx: 0,
-                s_2_idx: 0,
-                seq_len: 0,
-            },
-            &shared_sub_seq_data,
-            &mut |progress| {
-                max_len = max_len.max(progress.seq_len);
-            },
-        );
-
-        max_len
-    }
-
-    pub fn longest_common_subsequence(s_1: String, s_2: String) -> i32 {
-        let longest_subseq_grid = vec![vec![0; s_2.len()]; s_1.len()];
-
-        let (bigger_s, lesser_s) = if s_1.len() >= s_2.len() {
-            (&s_1, &s_2)
-        } else {
-            (&s_2, &s_1)
-        };
-
-        if bigger_s.contains(lesser_s) {
-            return lesser_s.len() as i32;
-        }
-
-        let bigger_s_chars: Vec<char> = bigger_s.chars().collect();
-        let lesser_s_chars: Vec<char> = lesser_s.chars().collect();
-
-        Self::recursive_sub_seq_search(&bigger_s_chars, &lesser_s_chars) as i32
+        *grid.last().unwrap().last().unwrap() as i32
     }
 }
 
@@ -170,6 +92,6 @@ mod tests {
 
         let result = Solution::longest_common_subsequence(s_1, s_2);
 
-        assert_eq!(result, 0);
+        assert_eq!(result, 19);
     }
 }
