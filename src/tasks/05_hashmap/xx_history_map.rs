0665714156fc65aac397fcd21285178c2df57368
Vitaly Senko <senko.vitaly@gmail.com>
2025-01-19 00:54:04 +0100

    Make historical hashmap more agile.

--- src/tasks/05_hashmap/xx_history_map.rs
+++ src/tasks/05_hashmap/xx_history_map.rs
@@ -1,7 +1,7 @@
 //! History map
-//! Write map that holds only last N accessed elements
+//! Write map that holds only last N accessed/inserted elements
+
 use std::collections::{HashMap, VecDeque};
-use std::hash::Hash;
 
 struct HistoryMap<K, T> {
     max_history: usize,
@@ -11,7 +11,7 @@ struct HistoryMap<K, T> {
 
 impl<K, T> HistoryMap<K, T>
 where
-    K: Eq + Hash + Clone,
+    K: Eq + std::hash::Hash + Clone,
 {
     pub fn new(max_history: usize) -> Self {
         Self {
@@ -21,7 +21,7 @@ where
         }
     }
 
-    fn _check_history(&mut self, current_op_key: &K) {
+    fn _update_history(&mut self, current_op_key: &K) {
         let new_k: K = if self.history.front().is_some_and(|fk| fk == current_op_key) {
             self.history.pop_front().unwrap()
         } else {
@@ -29,23 +29,47 @@ where
         };
 
         self.history.push_back(new_k);
+    }
 
-        if self.history.len() > self.max_history {
+    fn _ensure_max_history_treshold(&mut self) {
+        while self.history.len() > self.max_history {
             if let Some(key_to_remove) = self.history.pop_front() {
                 self.map.remove(&key_to_remove);
+            } else {
+                break;
             }
         }
     }
 
     pub fn get(&mut self, key: &K) -> Option<&T> {
-        // can be adjusted to check or not with future options
-        //self._check_history(key);
+        if self.map.contains_key(key) {
+            self._update_history(key);
+            self._ensure_max_history_treshold();
+        }
+
+        self.map.get(key)
+    }
+
+    pub fn get_untriggered(&mut self, key: &K) -> Option<&T> {
+        if self.map.contains_key(key) {
+            self._update_history(key);
+        }
+
+        self.map.get(key)
+    }
+
+    pub fn get_untraced(&mut self, key: &K) -> Option<&T> {
         self.map.get(key)
     }
 
     pub fn insert(&mut self, key: K, value: T) {
-        // can be adjusted to check or not with future options
-        self._check_history(&key);
+        self._update_history(&key);
+        self._ensure_max_history_treshold();
+        self.map.insert(key, value);
+    }
+
+    pub fn insert_untriggered(&mut self, key: K, value: T) {
+        self._update_history(&key);
         self.map.insert(key, value);
     }
 }
@@ -55,7 +79,7 @@ mod tests {
     use super::HistoryMap;
 
     #[test]
-    fn history_map_1() {
+    fn history_map_trigger_on_insert() {
         let n = 3;
         let mut hm: HistoryMap<String, usize> = HistoryMap::new(n);
 
@@ -63,15 +87,39 @@ mod tests {
         hm.insert("key_2".into(), 2);
         hm.insert("key_3".into(), 3);
 
-        assert!(hm.get(&"key_1".into()).is_some());
-        assert!(hm.get(&"key_2".into()).is_some());
-        assert!(hm.get(&"key_3".into()).is_some());
+        assert!(hm.get_untraced(&"key_1".into()).is_some());
+        assert!(hm.get_untraced(&"key_2".into()).is_some());
+        assert!(hm.get_untraced(&"key_3".into()).is_some());
 
         hm.insert("key_4".into(), 4);
 
-        assert!(hm.get(&"key_1".into()).is_none());
-        assert!(hm.get(&"key_2".into()).is_some());
+        assert!(hm.get_untraced(&"key_1".into()).is_none());
+        assert!(hm.get_untraced(&"key_2".into()).is_some());
+        assert!(hm.get_untraced(&"key_3".into()).is_some());
+        assert!(hm.get_untraced(&"key_4".into()).is_some());
+    }
+
+    #[test]
+    fn history_map_trigger_on_get() {
+        let n = 3;
+        let mut hm: HistoryMap<String, usize> = HistoryMap::new(n);
+
+        hm.insert_untriggered("key_1".into(), 1);
+        hm.insert_untriggered("key_2".into(), 2);
+        hm.insert_untriggered("key_3".into(), 3);
+        hm.insert_untriggered("key_4".into(), 4);
+
+        assert!(hm.get_untraced(&"key_1".into()).is_some());
+        assert!(hm.get_untraced(&"key_2".into()).is_some());
+        assert!(hm.get_untraced(&"key_3".into()).is_some());
+        assert!(hm.get_untraced(&"key_4".into()).is_some());
+
+        // by this operation accessing "key_1" is the newest operation
+        // while "key_2" become the oldest one and out of max treshold.
+        assert!(hm.get(&"key_1".into()).is_some());
+        assert!(hm.get(&"key_2".into()).is_none());
         assert!(hm.get(&"key_3".into()).is_some());
         assert!(hm.get(&"key_4".into()).is_some());
+        assert!(hm.get(&"key_1".into()).is_some());
     }
 }
