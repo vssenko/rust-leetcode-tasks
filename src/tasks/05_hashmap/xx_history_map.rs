50f1aaf7a6d6c8f527b011d78e48dfc21c55db90
Vitaly Senko <senko.vitaly@gmail.com>
2025-01-20 03:38:25 +0100

    Finish history map by O(1).

--- src/tasks/05_hashmap/xx_history_map.rs
+++ src/tasks/05_hashmap/xx_history_map.rs
@@ -1,45 +1,276 @@
 //! History map
 //! Write map that holds only last N accessed/inserted elements
 
-use std::collections::{HashMap, VecDeque};
+use std::{
+    cell::RefCell,
+    collections::{HashMap, VecDeque},
+    fmt::{Debug, Pointer},
+    rc::Rc,
+};
+
+type NodeRef<T> = Rc<RefCell<GutsDequeNode<T>>>;
+
+struct GutsDequeNode<T> {
+    value: T,
+    next: Option<NodeRef<T>>,
+    prev: Option<NodeRef<T>>,
+}
+
+impl<T> GutsDequeNode<T> {
+    pub fn new(value: T, prev: Option<NodeRef<T>>, next: Option<NodeRef<T>>) -> Self {
+        GutsDequeNode { value, next, prev }
+    }
+
+    pub fn move_to_rc(self) -> NodeRef<T> {
+        Rc::new(RefCell::new(self))
+    }
+}
+
+struct GutsOutsideDeque<T> {
+    head: Option<NodeRef<T>>,
+    tail: Option<NodeRef<T>>,
+}
+
+impl<T> Debug for GutsDequeNode<T>
+where
+    T: std::fmt::Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("GutsDequeNode")
+            .field("value", &self.value)
+            .field(
+                "next",
+                &if self.next.is_some() {
+                    format!("Some({:?})", self.next.as_ref().unwrap().borrow().value)
+                } else {
+                    "None".to_string()
+                },
+            )
+            .field(
+                "prev",
+                &if self.prev.is_some() {
+                    format!("Some({:?})", self.prev.as_ref().unwrap().borrow().value)
+                } else {
+                    "None".to_string()
+                },
+            )
+            .finish()
+    }
+}
+
+impl<T> Debug for GutsOutsideDeque<T>
+where
+    T: std::fmt::Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut nodes_dbg: Vec<String> = Vec::new();
+
+        self.for_each(&mut |node| {
+            // finish push
+            nodes_dbg.push(format!("{:?}", node.borrow()))
+        });
+
+        write!(
+            f,
+            "GutsOutsideDeque {{ \nhead: {:?}, \ntail: {:?}, \nlist: [{}] }}",
+            self.head,
+            self.tail,
+            nodes_dbg.join(",")
+        )
+    }
+}
+
+impl<T> GutsOutsideDeque<T> {
+    pub fn new() -> Self {
+        Self {
+            head: None,
+            tail: None,
+        }
+    }
+
+    // much better to do through Iterator.
+    pub fn for_each<F: FnMut(&NodeRef<T>)>(&self, cb: &mut F) {
+        if let Some(head) = self.head.as_ref() {
+            // complete garbage, but i don't care
+            let mut walker: Option<NodeRef<T>> = Some(head.clone());
+            while let Some(wk) = walker {
+                cb(&wk);
+                walker = wk.borrow().next.clone();
+            }
+        }
+    }
+
+    pub fn push_back(&mut self, item: T) {
+        let item_rc = GutsDequeNode::new(item, self.tail.clone(), None).move_to_rc();
+        match &self.tail {
+            Some(tail) => {
+                tail.borrow_mut().next = Some(item_rc.clone());
+                self.tail = Some(item_rc);
+            }
+            None => self.tail = Some(item_rc),
+        }
+        if self.head.is_none() {
+            self.head = self.tail.as_mut().map(|r| r.clone());
+        }
+    }
+
+    pub fn push_front(&mut self, item: T) {
+        let item_rc = GutsDequeNode::new(item, None, self.head.clone()).move_to_rc();
+        match self.head.as_mut() {
+            Some(head) => {
+                head.borrow_mut().prev = Some(item_rc.clone());
+                self.head = Some(item_rc);
+            }
+            None => self.head = Some(item_rc),
+        }
+        if self.tail.is_none() {
+            self.tail = self.head.as_mut().map(|r| r.clone());
+        }
+    }
+
+    // yes, this is ugly.
+    pub fn front(&self) -> Option<&NodeRef<T>> {
+        self.head.as_ref()
+    }
+
+    // yes, this is ugly too.
+    pub fn back(&self) -> Option<&NodeRef<T>> {
+        self.tail.as_ref()
+    }
+
+    pub fn pop_front(&mut self) -> Option<NodeRef<T>> {
+        let head_item = match self.head.take() {
+            Some(head) => {
+                let next = head.borrow().next.clone();
+                self.head = next;
+                if self.head.is_some() {
+                    self.head.as_ref().unwrap().borrow_mut().prev = None;
+                }
+                Some(head)
+            }
+            None => None,
+        };
+
+        if self.head.is_none() {
+            self.tail = None;
+        }
+
+        head_item
+    }
+
+    pub fn pop_back(&mut self) -> Option<NodeRef<T>> {
+        let tail_item = match self.tail.take() {
+            Some(tail) => {
+                let prev = tail.borrow().prev.clone();
+                self.tail = prev;
+                if self.tail.is_some() {
+                    self.tail.as_ref().unwrap().borrow_mut().next = None;
+                }
+                Some(tail)
+            }
+            None => None,
+        };
+
+        if self.tail.is_none() {
+            self.head = None;
+        }
+
+        tail_item
+    }
+
+    // all of that was just for this...
+    pub fn move_to_back(&mut self, node: &NodeRef<T>) {
+        let is_head = self
+            .head
+            .as_ref()
+            .is_some_and(|h| h.as_ptr() == node.as_ptr());
+        if is_head && node.borrow().next.is_some() {
+            self.head = node.borrow().next.clone();
+        }
+
+        let original_prev = node.borrow().prev.clone();
+        let original_next = node.borrow().next.clone();
+
+        if original_prev.is_some() {
+            original_prev.as_ref().unwrap().borrow_mut().next = original_next.clone();
+        }
+
+        if original_next.is_some() {
+            if is_head {
+                original_next.unwrap().borrow_mut().prev = None;
+            } else {
+                original_next.unwrap().borrow_mut().prev = original_prev.clone();
+            }
+        }
+
+        let mut bor_muted = node.borrow_mut();
+        bor_muted.prev = self.tail.clone();
+        bor_muted.next = None;
+
+        if (self.tail.is_some()) {
+            self.tail.as_ref().unwrap().borrow_mut().next = Some(node.clone());
+        }
+        self.tail = Some(node.clone());
+    }
+}
+
+struct HistoryMapItem<K, T> {
+    item: T,
+    history_node: NodeRef<K>,
+}
 
 struct HistoryMap<K, T> {
     max_size: usize,
-    map: HashMap<K, T>,
-    history: VecDeque<K>,
+    map: HashMap<K, HistoryMapItem<K, T>>,
+    history: GutsOutsideDeque<K>,
 }
 
 impl<K, T> HistoryMap<K, T>
 where
     K: Eq + std::hash::Hash + Clone + std::fmt::Debug,
+    T: std::fmt::Debug,
 {
     pub fn new(max_size: usize) -> Self {
         Self {
-            history: VecDeque::new(),
+            history: GutsOutsideDeque::new(),
             map: HashMap::new(),
             max_size,
         }
     }
 
-    fn _update_history(&mut self, current_op_key: &K) {
-        // completely not optimized solution
-        // which is implemented only to cover tests correctly
-        // to update afterwards to O(1)
+    fn _get_oldest_from_history(&mut self) -> Option<K> {
+        self.history
+            .pop_front()
+            .map(|node| node.borrow().value.clone())
+    }
 
-        let new_history: VecDeque<K> = self
+    fn _update_existing_history_item(&mut self, current_op_key: &K) {
+        if self
             .history
-            .iter()
-            .filter(|&k| !k.eq(current_op_key))
-            .cloned()
-            .collect();
+            .back()
+            .is_some_and(|bk| bk.borrow().value.eq(current_op_key))
+        {
+            return;
+        }
 
-        self.history = new_history;
-        self.history.push_back(current_op_key.clone());
+        if self
+            .history
+            .front()
+            .is_some_and(|bk| bk.borrow().value.eq(current_op_key))
+        {
+            let front_node = self.history.pop_front().unwrap();
+
+            self.history.push_back(front_node.borrow().value.clone());
+            return;
+        }
+
+        let node = &self.map[current_op_key].history_node;
+        self.history.move_to_back(node);
     }
 
     fn _ensure_max_size_treshold(&mut self) {
         while self.map.len() > self.max_size {
-            if let Some(key_to_remove) = self.history.pop_front() {
+            if let Some(key_to_remove) = self._get_oldest_from_history() {
                 self.map.remove(&key_to_remove);
             } else {
                 break;
@@ -49,32 +280,50 @@ where
 
     pub fn get(&mut self, key: &K) -> Option<&T> {
         if self.map.contains_key(key) {
-            self._update_history(key);
+            self._update_existing_history_item(key);
             self._ensure_max_size_treshold();
         }
-        self.map.get(key)
+        self.map.get(key).map(|n| &n.item)
     }
 
     pub fn get_untriggered(&mut self, key: &K) -> Option<&T> {
         if self.map.contains_key(key) {
-            self._update_history(key);
+            self._update_existing_history_item(key);
         }
-        self.map.get(key)
+        self.map.get(key).map(|n| &n.item)
     }
 
     pub fn get_untraced(&mut self, key: &K) -> Option<&T> {
-        self.map.get(key)
+        self.map.get(key).map(|n| &n.item)
     }
 
-    pub fn put(&mut self, key: K, value: T) {
-        self._update_history(&key);
-        self.map.insert(key, value);
-        self._ensure_max_size_treshold();
+    #[allow(clippy::map_entry)]
+    fn _put(&mut self, key: K, value: T, ensure: bool) {
+        if self.map.contains_key(&key) {
+            self._update_existing_history_item(&key);
+            self.map.entry(key).and_modify(|item| item.item = value);
+        } else {
+            self.history.push_back(key.clone());
+            let new_node = self.map.insert(
+                key,
+                HistoryMapItem {
+                    item: value,
+                    history_node: self.history.tail.as_ref().unwrap().clone(),
+                },
+            );
+        }
+
+        if (ensure) {
+            self._ensure_max_size_treshold();
+        }
+    }
+
+    fn put(&mut self, key: K, value: T) {
+        self._put(key, value, true);
     }
 
     pub fn put_untriggered(&mut self, key: K, value: T) {
-        self._update_history(&key);
-        self.map.insert(key, value);
+        self._put(key, value, false);
     }
 }
 
@@ -128,9 +377,30 @@ mod tests {
         hm.put("key_1".into(), 1);
         hm.put("key_2".into(), 2);
         hm.put("key_3".into(), 3);
+
+        assert!(hm.get_untraced(&"key_1".into()).is_some());
+        assert!(hm.get_untraced(&"key_2".into()).is_some());
+        assert!(hm.get_untraced(&"key_3".into()).is_some());
+
         hm.put("key_2".into(), 22);
+
+        assert!(hm.get_untraced(&"key_1".into()).is_some());
+        assert!(hm.get_untraced(&"key_2".into()).is_some());
+        assert!(hm.get_untraced(&"key_3".into()).is_some());
+
         hm.put("key_1".into(), 11);
+
+        assert!(hm.get_untraced(&"key_1".into()).is_some());
+        assert!(hm.get_untraced(&"key_2".into()).is_some());
+        assert!(hm.get_untraced(&"key_3".into()).is_some());
+
         hm.put("key_4".into(), 4);
+
+        assert!(hm.get_untraced(&"key_1".into()).is_some());
+        assert!(hm.get_untraced(&"key_2".into()).is_some());
+        assert!(hm.get_untraced(&"key_3".into()).is_none());
+        assert!(hm.get_untraced(&"key_4".into()).is_some());
+
         hm.put("key_5".into(), 5);
 
         assert!(hm.get_untraced(&"key_1".into()).is_some());
